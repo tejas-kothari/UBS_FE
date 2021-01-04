75408b01b8fad4db161e0b44b99edb7f352e6e51
samsonwong522 <samsonwong522@gmail.com>
2021-01-04 15:53:15 +0800

    Insert chart from tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Button } from "@material-ui/core";
 import "./App.css";
+import ChartWrapper from "./chart/ChartWrapper";
 
 function App() {
   return (
     <Box display="flex" width="100vw" height="100vh" alignItems="center" justifyContent="center">
-      <Button color="primary">Hello World</Button>
+      <ChartWrapper />
     </Box>
   );
 }
