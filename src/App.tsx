188e83a7ee50941cf4da264455bece17e8c78773
samsonwong522 <samsonwong522@gmail.com>
2021-01-04 22:16:36 +0800

    Add more demo charts

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import "./App.css";
 import ChartWrapper from "./chart/ChartWrapper";
 import TestChart from "./chart/TestChart";
 import TestChart2 from "./chart/TestChart2";
+import TestChart3 from "./chart/TestChart3";
+import TestChart4 from "./chart/TestChart4";
 
 function App() {
   return (
     <Box display="flex" width="100vw" alignItems="center" justifyContent="center" flexWrap="wrap">
       {ChartWrapper<TestChart>(TestChart)}
       {ChartWrapper<TestChart2>(TestChart2)}
-      {ChartWrapper<TestChart>(TestChart)}
-      {ChartWrapper<TestChart>(TestChart)}
-      {ChartWrapper<TestChart>(TestChart)}
-      {ChartWrapper<TestChart>(TestChart)}
+      {ChartWrapper<TestChart3>(TestChart3)}
+      {ChartWrapper<TestChart4>(TestChart4)}
       {/* <Typography variant="h2" gutterBottom>
         Funding by Year
       </Typography> */}
