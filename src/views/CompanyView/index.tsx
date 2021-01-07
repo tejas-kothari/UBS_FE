92916c7dc8f234e53f2824009a6839038e527581
samsonwong522 <samsonwong522@gmail.com>
2021-01-07 21:53:17 +0800

    Show company name in CompanyView

--- src/views/CompanyView/index.tsx
+++ src/views/CompanyView/index.tsx
@@ -4,6 +4,7 @@ import Page from '../../components/Page';
 import Typography from '@material-ui/core/Typography';
 import 'fontsource-roboto';
 import { useParams } from 'react-router-dom';
+import companiesData from '../../tmp_data/companies_data.json';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,13 +16,15 @@ const useStyles = makeStyles((theme) => ({
 
 function CompanyView() {
   const classes = useStyles();
+
   const { companyId } = useParams();
+  const company = companiesData.find(company => company.uuid === companyId);
 
   return (
     <Page title="Portfolio" className={classes.root}>
       
       <Typography variant="h1" >
-        Company: {companyId}
+        Company: {company?.name}
       </Typography>
       
 
