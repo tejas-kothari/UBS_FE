5f884411e2fd7a68a5dee03f6cd21d79b2d95621
samsonwong522 <samsonwong522@gmail.com>
2021-01-08 15:37:34 +0800

    Change CompanyView to use new dataset

--- src/views/CompanyView/index.tsx
+++ src/views/CompanyView/index.tsx
@@ -1,12 +1,13 @@
 import { makeStyles } from '@material-ui/core';
-import React from 'react';
-import Page from '../../components/Page';
 import Typography from '@material-ui/core/Typography';
 import 'fontsource-roboto';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import companiesData from '../../tmp_data/companies_data.json';
+import Page from '../../components/Page';
+import { Company } from '../../interfaces/company';
+import companiesData from '../../tmp_data/crunchbase_data_heads/organizations.json';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.background.default,
     minHeight: '100%',
@@ -18,16 +19,14 @@ function CompanyView() {
   const classes = useStyles();
 
   const { companyId } = useParams();
-  const company = companiesData.find(company => company.uuid === companyId);
+  const company = companiesData.find(
+    company => company.uuid === companyId
+  ) as Company;
+  console.log(companiesData);
 
   return (
     <Page title="Portfolio" className={classes.root}>
-      
-      <Typography variant="h1" >
-        Company: {company?.name}
-      </Typography>
-      
-
+      <Typography variant="h1">Company: {company?.name}</Typography>
     </Page>
   );
 }
