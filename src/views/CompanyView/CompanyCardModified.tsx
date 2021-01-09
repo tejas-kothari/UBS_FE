a42e245cf9d3a8fb8fd3a2b9685757607758fb4c
samsonwong522 <samsonwong522@gmail.com>
2021-01-09 12:20:44 +0800

    Beautify design of CompanyView

--- src/views/CompanyView/CompanyCardModified.tsx
+++ src/views/CompanyView/CompanyCardModified.tsx
@@ -1,128 +1,120 @@
 import {
-    Card,
-    CardActions,
-    CardContent,
-    Grid,
-    IconButton,
-    makeStyles,
-    Typography
-  } from '@material-ui/core';
-  
-  import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-  import CategoryIcon from '@material-ui/icons/Category';
-  import LanguageIcon from '@material-ui/icons/Language';
-  import LocationOnIcon from '@material-ui/icons/LocationOn';
-  import PeopleIcon from '@material-ui/icons/People';
-  import ShowChartIcon from '@material-ui/icons/ShowChart';
-  import React from 'react';
-  import { NavLink } from 'react-router-dom';
-  import { Company } from '../../interfaces/company';
-  
-  const useStyles = makeStyles(theme => ({
-    root: {
-      display: 'flex'
-    },
-    title: {
-      fontSize: 14
-    },
-    spacer: {
-      flexGrow: 1
-    },
-    cardContent: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center'
-    },
-    icon: {
-      verticalAlign: 'middle',
-      marginRight: theme.spacing(1)
-    },
-    details: {
-      maxWidth: 600,
-      width: '100%'
-    }
-  }));
-  
-  type CompanyCardProps = {
-    company: Company;
-  };
-  
-  function CompanyCardModified({ company }: CompanyCardProps) {
-    const classes = useStyles();
-  
-    return (
-      <Grid item>
-        <Card className={classes.root}>
-          <CardContent className={classes.cardContent}>
-            <Grid container spacing={2} alignItems="center">
-              
-              <Grid item>
-                <img src={company.logo_url} alt={company.name} />
-              </Grid>
-              <Grid item>
-                <Typography
-                  className={classes.title}
-                  color="textPrimary"
-                  variant="h1"
-                  gutterBottom
-                >
-                  {company.name}
-                </Typography>
-                <Grid container spacing={1} className={classes.details}>
-                  <Grid item xs={6}>
-                    <Typography noWrap={true}>
-                      <CategoryIcon className={classes.icon} />
-                      {company.category_list.split(',')[0] || 'unknown'}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography noWrap={true}>
-                      <LocationOnIcon className={classes.icon} />
-                      {company.country_code || 'unknown'}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography noWrap={true}>
-                      <PeopleIcon className={classes.icon} />
-                      {company.employee_count || 'unknown'}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography noWrap={true}>
-                      <ShowChartIcon className={classes.icon} />
-                      {company.num_funding_rounds || 'unknown'}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography noWrap={true}>
-                      <AttachMoneyIcon className={classes.icon} />
-                      USD {company.total_funding_usd || 'unknown'}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography noWrap={true}>
-                      <LanguageIcon className={classes.icon} />
-                      {company.homepage_url || 'unknown'}
-                    </Typography>
-                  </Grid>
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  IconButton,
+  makeStyles,
+  Typography
+} from '@material-ui/core';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import CategoryIcon from '@material-ui/icons/Category';
+import LanguageIcon from '@material-ui/icons/Language';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PeopleIcon from '@material-ui/icons/People';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Company } from '../../interfaces/company';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  title: {
+    fontSize: '1.25rem',
+    fontWeight: 'bold'
+  },
+  spacer: {
+    flexGrow: 1
+  },
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  icon: {
+    verticalAlign: 'middle',
+    marginRight: theme.spacing(1)
+  },
+  details: {
+    maxWidth: 600,
+    width: '100%'
+  }
+}));
+
+type CompanyCardProps = {
+  company: Company;
+};
+
+function CompanyCardModified({ company }: CompanyCardProps) {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={12}>
+      <Card className={classes.root}>
+        <CardContent className={classes.cardContent}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <img src={company.logo_url} alt={company.name} />
+            </Grid>
+            <Grid item>
+              <Typography className={classes.title} gutterBottom>
+                {company.name}
+              </Typography>
+              <Grid container spacing={1} className={classes.details}>
+                <Grid item xs={6}>
+                  <Typography noWrap={true} color="textSecondary">
+                    <CategoryIcon className={classes.icon} />
+                    {company.category_list.split(',')[0] || 'unknown'}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography noWrap={true} color="textSecondary">
+                    <LocationOnIcon className={classes.icon} />
+                    {company.country_code || 'unknown'}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography noWrap={true} color="textSecondary">
+                    <PeopleIcon className={classes.icon} />
+                    {company.employee_count || 'unknown'}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography noWrap={true} color="textSecondary">
+                    <ShowChartIcon className={classes.icon} />
+                    {company.num_funding_rounds || 'unknown'}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography noWrap={true} color="textSecondary">
+                    <AttachMoneyIcon className={classes.icon} />
+                    USD {company.total_funding_usd || 'unknown'}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography noWrap={true} color="textSecondary">
+                    <LanguageIcon className={classes.icon} />
+                    {company.homepage_url || 'unknown'}
+                  </Typography>
                 </Grid>
               </Grid>
             </Grid>
-          </CardContent>
-          <div className={classes.spacer}></div>
-          <CardActions>
-            <IconButton
-              component={NavLink}
-              to={`/companies/${company.uuid}`}
-              size="small"
-            >
-            
-            </IconButton>
-          </CardActions>
-        </Card>
-      </Grid>
-    );
-  }
-  
-  export default CompanyCardModified;
-  
+          </Grid>
+        </CardContent>
+        <div className={classes.spacer}></div>
+        <CardActions>
+          <IconButton
+            component={NavLink}
+            to={`/companies/${company.uuid}`}
+            size="small"
+          ></IconButton>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+}
+
+export default CompanyCardModified;
--- src/views/CompanyView/index.tsx
+++ src/views/CompanyView/index.tsx
@@ -1,15 +1,7 @@
+import classes from '*.module.css';
 import { Box, makeStyles } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import 'fontsource-roboto';
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Page from '../../components/Page';
-import { Company } from '../../interfaces/company';
-import companiesData from '../../tmp_data/crunchbase_data_heads/organizations.json';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import 'fontsource-roboto';
-import CompanyCardModified from '../CompanyView/CompanyCardModified';
+import Checkbox /* CheckboxProps */ from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 /*import { withStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
@@ -17,10 +9,18 @@ import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder'; */
 import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox, {/* CheckboxProps */} from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import 'fontsource-roboto';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import ChartWrapper from '../../chart/ChartWrapper';
 import TestChart3 from '../../chart/TestChart3';
+import Page from '../../components/Page';
+import { Company } from '../../interfaces/company';
+import companiesData from '../../tmp_data/crunchbase_data_heads/organizations.json';
+import CompanyCardModified from '../CompanyView/CompanyCardModified';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,6 +32,13 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3),
     textAlign: 'center',
     color: theme.palette.text.primary
+  },
+  checkboxLabel: {
+    textAlign: 'left'
+  },
+  title: {
+    fontSize: '1.25rem',
+    fontWeight: 'bold'
   }
 }));
 
@@ -45,48 +52,6 @@ const useStyles = makeStyles(theme => ({
   checked: {},
 })((props: CheckboxProps) => <Checkbox color="default" {...props} />);
 */
-function CheckboxLabels() {
-  const [state, setState] = React.useState({
-    checkedA: false,
-    checkedB: false,
-    checkedC: false,
-    checkedD: false,
-    checkedE: false,
-    checkedF: false,
-    checkedG: false,
-    checkedH: false,
-    checkedI: false,
-    checkedJ: false,
-    checkedK: false,
-    checkedL: false,
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
-
-  return (
-    <FormGroup row>
-           
-      <FormControlLabel control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedA" />}label="Number of Employees"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedB} onChange={handleChange} name="checkedB" />}label="Number of News Articles"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedC} onChange={handleChange} name="checkedC" />}label="Number of Competitors"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedD} onChange={handleChange} name="checkedD" />}label="Number of Offices"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedE} onChange={handleChange} name="checkedE" />}label="Number of Products"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedF} onChange={handleChange} name="checkedF" />}label="Total Investments"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedG} onChange={handleChange} name="checkedG" />}label="Total Number of Acquisitions"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedH} onChange={handleChange} name="checkedH" />}label="Number of Investments"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedI} onChange={handleChange} name="checkedI" />}label="Number of Total Investments"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedJ} onChange={handleChange} name="checkedJ" />}label="Total Number of companies by same founder"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedK} onChange={handleChange} name="checkedK" />}label="??"/>
-      <FormControlLabel control={<Checkbox checked={state.checkedL} onChange={handleChange} name="checkedL" />}label="???"/>
-
-
-
-      
-    </FormGroup>
-  );
-}
 
 function CompanyView() {
   const classes = useStyles();
@@ -97,55 +62,155 @@ function CompanyView() {
   ) as Company;
   console.log(companiesData);
 
+  function CheckboxLabels() {
+    const [state, setState] = React.useState({
+      checkedA: false,
+      checkedB: false,
+      checkedC: false,
+      checkedD: false,
+      checkedE: false,
+      checkedF: false,
+      checkedG: false,
+      checkedH: false,
+      checkedI: false,
+      checkedJ: false,
+      checkedK: false,
+      checkedL: false
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
+    function CheckboxLabel({
+      value,
+      name,
+      label
+    }: {
+      value: boolean;
+      name: string;
+      label: string;
+    }) {
+      return (
+        <Grid item xs={12} sm={6} md={3}>
+          <FormControlLabel
+            control={
+              <Checkbox checked={value} onChange={handleChange} name={name} />
+            }
+            label={label}
+            className={classes.checkboxLabel}
+          />
+        </Grid>
+      );
+    }
+
+    return (
+      <Grid container>
+        <CheckboxLabel
+          name="checkedA"
+          value={state.checkedA}
+          label="Number of Employees"
+        />
+        <CheckboxLabel
+          name="checkedB"
+          value={state.checkedB}
+          label="Number of News Articles"
+        />
+        <CheckboxLabel
+          name="checkedC"
+          value={state.checkedC}
+          label="Number of Competitors"
+        />
+
+        <CheckboxLabel
+          name="checkedD"
+          value={state.checkedD}
+          label="Number of Offices"
+        />
+
+        <CheckboxLabel
+          name="checkedE"
+          value={state.checkedE}
+          label="Number of Products"
+        />
+
+        <CheckboxLabel
+          name="checkedF"
+          value={state.checkedF}
+          label="Total Investments"
+        />
+
+        <CheckboxLabel
+          name="checkedG"
+          value={state.checkedG}
+          label="Total Number of Acquisitions"
+        />
+
+        <CheckboxLabel
+          name="checkedH"
+          value={state.checkedH}
+          label="Number of Investments"
+        />
+
+        <CheckboxLabel
+          name="checkedI"
+          value={state.checkedI}
+          label="Number of Total Investments"
+        />
+
+        <CheckboxLabel
+          name="checkedJ"
+          value={state.checkedJ}
+          label="Total Number of companies by same founder"
+        />
+
+        <CheckboxLabel name="checkedK" value={state.checkedK} label="??" />
+
+        <CheckboxLabel name="checkedL" value={state.checkedL} label="???" />
+      </Grid>
+    );
+  }
+
   return (
-    <Page title="Portfolio" className={classes.root}>
+    <Page title={company.name} className={classes.root}>
       <Typography variant="h1">Company View </Typography>
       <Typography variant="body1">&nbsp;</Typography>
-      <Grid container
+      <Grid
+        container
         spacing={1}
         direction="row"
         justify="flex-start"
-        alignItems="flex-start">
-        
-
-        <Paper>
+        alignItems="flex-start"
+      >
+        <Grid item xs={12}>
+          <CompanyCardModified company={company} key={company.uuid} />
+        </Grid>
+        <Grid xs={12}>
+          <Typography variant="body1">&nbsp;</Typography>
+        </Grid>
+        <Paper className={classes.paper}>
           <Grid item xs={12}>
-            <CompanyCardModified company={company} key={company.uuid} />
+            <Typography variant="body1" align="left" className={classes.title}>
+              Data Features
+            </Typography>
+            <CheckboxLabels />
           </Grid>
         </Paper>
         <Grid xs={12}>
           <Typography variant="body1">&nbsp;</Typography>
-          
         </Grid>
-        <Paper className={classes.paper}>
-          <Grid item xs={12}>
-            <Typography variant="body1" align='left'>
-              Data Features
-            </Typography>
-            <CheckboxLabels/>
-          </Grid>
-            
-        </Paper> 
-        <Grid xs={12}>
-          <Typography variant="body1">&nbsp;</Typography>
-          
-        </Grid>  
         <Paper>
           <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexWrap="wrap"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexWrap="wrap"
           >
             {ChartWrapper<TestChart3>(TestChart3)}
           </Box>
-           
-        </Paper>  
+        </Paper>
       </Grid>
     </Page>
-
-      
-    
   );
 }
 
