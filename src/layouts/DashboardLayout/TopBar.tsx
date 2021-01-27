085be903ba783697b9fedd44b94352b7b5cf4386
samsonwong522 <samsonwong522@gmail.com>
2021-01-27 20:45:23 +0800

    Fix TopBar link

--- src/layouts/DashboardLayout/TopBar.tsx
+++ src/layouts/DashboardLayout/TopBar.tsx
@@ -47,20 +47,6 @@ const TopBar = ({ className, onMobileNavOpen, ...rest }: TopBarProps) => {
           <img src="/static/ubs-logo-svg.svg" alt="UBS Team 1" height="50px" />
         </RouterLink>
         <Box flexGrow={1} />
-        {/* <Hidden mdDown>
-          <IconButton color="inherit">
-            <Badge
-              badgeContent={notifications.length}
-              color="primary"
-              variant="dot"
-            >
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton color="inherit">
-            <InputIcon />
-          </IconButton>
-        </Hidden> */}
         <Hidden lgUp>
           <IconButton color="secondary" onClick={onMobileNavOpen} className={classes.menuBtn}>
             <MenuIcon />
--- src/routes.tsx
+++ src/routes.tsx
@@ -43,7 +43,7 @@ const routes = [
       },
       {
         path: '/',
-        element: <Navigate to="/companies/featured" />
+        element: <Navigate to="/companies/list" />
       }
     ]
   },
